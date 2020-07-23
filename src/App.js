99718dad0144f001c70d0ff32b3dd6a695bf71dd
lalebdi <loai.alebdi@gmail.com>
2020-07-23 09:18:04 -0400

    initial layout and assets added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <h1>The Nautilus Project</h1>
+      <NavBar />
     </div>
   );
 }
