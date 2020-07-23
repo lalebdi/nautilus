5a9cd071baf39c8cbe1041265bf66aab71e6bec6
lalebdi <loai.alebdi@gmail.com>
2020-07-23 09:29:19 -0400

    started with the logo and NavBar layout

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react'
+import logo from '../assets/nautilus-logo-sprite@2x.png';
 
 export default class NavBar extends Component {
     render() {
         return (
-            <div>
-                
+            <div className="big-wrapper">
+                <div className="left-side"></div>
+                <img src={logo} alt="logo" className="logo" />
+                <div className="right-side"></div>
             </div>
         )
     }
